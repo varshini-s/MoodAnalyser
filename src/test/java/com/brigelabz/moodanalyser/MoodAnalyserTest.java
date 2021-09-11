d8d05863dccbf7e0f6b1f29bfd779cd7aa189b86
varshini <dreamvarshini@gmail.com>
2021-09-11 16:48:16 +0530

    REFACTORED: type of exception added using enum to test cases

--- src/test/java/com/brigelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/brigelabz/moodanalyser/MoodAnalyserTest.java
@@ -6,6 +6,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.brigelabz.moodanalyser.MoodAnalysisException.ExceptionType;
+
 
 public class MoodAnalyserTest {
 
@@ -13,51 +15,31 @@ public class MoodAnalyserTest {
 	public void givenMessage_WhenSad_ShouldReturnSad() 
 	{
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood;
-		try 
-		{
-			mood = moodAnalyser.analyseMood("This is a sad Message");
-			assertEquals("SAD", mood);
-		}
-		catch (MoodAnalysisException e) 
-		{
-
-			e.printStackTrace();
-		}
-
+		String mood = moodAnalyser.analyseMood("This is a sad Message");
+		assertEquals("SAD", mood);
 	}
 
 	@Test
 	public void givenMessage_WhenNoSad_ShouldReturnHappy() 
 	{
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood;
-		try 
-		{
-			mood = moodAnalyser.analyseMood("This is a happy Message");
-			assertEquals("HAPPY", mood);
-		} catch (MoodAnalysisException e) 
-		{
-
-			e.printStackTrace();
-		}
-
+		String mood = moodAnalyser.analyseMood("This is a happy Message");
+		assertEquals("HAPPY", mood);
 	}
+
 
 	@Test
 	public void givenNullMood_ShouldThrowException() 
 	{
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood;
-		try
+
+		try 
 		{
-			ExpectedException exceptionRule = ExpectedException.none();
-			exceptionRule.expect(MoodAnalysisException.class);
-			mood = moodAnalyser.analyseMood(null);
+			moodAnalyser.analyseMood(null);
 
 		} catch (MoodAnalysisException e) 
 		{
-			e.printStackTrace();
+			Assert.assertEquals(ExceptionType.ENTERED_NULL ,e.type);
 		}
 
 	}
@@ -66,16 +48,14 @@ public class MoodAnalyserTest {
 	public void givenEmptyMood_ShouldThrowException() 
 	{
 		MoodAnalyser moodAnalyser = new MoodAnalyser();
-		String mood;
-		try
+
+		try 
 		{
-			ExpectedException exceptionRule = ExpectedException.none();
-			exceptionRule.expect(MoodAnalysisException.class);
-			mood = moodAnalyser.analyseMood("");
+			moodAnalyser.analyseMood("");
 
 		} catch (MoodAnalysisException e) 
 		{
-			e.printStackTrace();
+			Assert.assertEquals(ExceptionType.ENTERED_EMPTY ,e.type);
 		}
 
 	}
